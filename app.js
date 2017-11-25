48df526c6a11e4036c4a27589903df7749357455
sebastian0987 <sra012@alumnos.ucn.cl>
2017-11-25 14:08:51 -0300

    UPDATE: PASSPORT-FACEBOOK Se agregaron los paquetes necesarios para iniciar sesion por medio de facebook, ademas se agregaron las principales funcionalidades para el correcto funcionamiento de estos paquetes

--- app.js
+++ app.js
@@ -6,9 +6,29 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 const path = require('path');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
 
 var app = express();
 
+//PASSPORT FACEBOOK
+app.use(cookieParser());
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+passport.serializeUser(function (user, done) {
+    done(null, user); // req.user
+});
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 
 //middlewares de body parser
@@ -23,15 +43,14 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 
 //Ruta Base
-app.use('/api', usuario_routes);
+app.use(usuario_routes);
 
 // Send all other requests to the Angular app
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
 //rutas body-parser
 
-
+// require('./server/passport')(app);
 
 module.exports = app;
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,6 +5,8 @@
 
 var express = require('express');
 var UsuarioController = require('../controllers/usuario');
+var passportFacebook = require('passport'),
+    facebookStrategy = require('passport-facebook');
 
 var api = express.Router();
 
@@ -12,5 +14,21 @@ api.get('/prueba', UsuarioController.prueba_usuario);
 api.post('/registro', UsuarioController.guardar_usuario);
 api.get('/buscar', UsuarioController.buscar_usuario);
 api.post('/iniciarSesion', UsuarioController.inicio_sesion);
+api.post('/login',UsuarioController.login);
+api.get('/registrar-facebook',passportFacebook.authenticate('facebook'));
+api.get('/auth/facebook/callback/',passportFacebook.authenticate('facebook',
+    { successRedirect: 'http://localhost:4200/',
+        failureRedirect: 'http://localhost:4200/login' }));
+
+passportFacebook.use(new facebookStrategy({
+        clientID: '1849209735108263',
+        clientSecret: '4b967610104a42ce7841eeec2e785795',
+        callbackURL: '/auth/facebook/callback/',
+    },
+    function (accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        UsuarioController.registrarFacebook(accessToken, refreshToken, profile, done);
+    }
+));
 
 module.exports = api;
