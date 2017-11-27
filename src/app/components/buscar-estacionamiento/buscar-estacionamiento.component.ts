6d91cef0ed47248193529e445a9c4534f3a479f3
Luis San Martín <lsansaa@gmail.com>
2017-11-27 16:01:45 -0300

    Corregido error al buscar lat y long con la dirección usando la api de google. El mapa cambia de origen dependiendo de la direccion ingresada

---
+++ src/app/components/buscar-estacionamiento/buscar-estacionamiento.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { GoogleMapsApiService } from "../../services/google-maps-api.service";
+
+@Component({
+  selector: 'app-buscar-estacionamiento',
+  templateUrl: './buscar-estacionamiento.component.html',
+  styleUrls: ['./buscar-estacionamiento.component.css']
+})
+export class BuscarEstacionamientoComponent implements OnInit {
+
+    title: string = 'My first AGM project';
+    origin  = {lat : -23.6509279, lng : -70.39750219999999};
+    mark = {lat : -23.6509279, lng : -70.39750219999999};
+
+  constructor( private GoogleMapsApiService: GoogleMapsApiService) { }
+
+  ngOnInit() {
+  }
+
+  getCoordinates(address){
+      let replaced = address.replace(/\s/g, '+');
+      console.log(replaced)
+      let latLong = this.GoogleMapsApiService.getCoordinates(address).subscribe((response)=>{
+          console.log(response);
+      });
+
+  }
+  findParking(address){
+      let replaced = address.replace(/\s/g, '+');
+      //console.log(replaced)
+      let latLong = this.GoogleMapsApiService.getCoordinates(address).subscribe((response)=>{
+          let lat = response['results'][0]['geometry']['location']['lat'];
+          let lng = response['results'][0]['geometry']['location']['lng'];
+          console.log(lat);
+          console.log(lng);
+          this.origin.lat = lat;
+          this.origin.lng = lng;
+      });
+  }
+
+}
