9d268fc73550643fd5a5639f8d9fa2408283dc2d
blacksinx <tas001@alumnos.ucn.cl>
2017-11-27 23:46:01 -0300

    Added: Se agrega la clase administrador.js con el metodo upgradeAdmin para cambiar un usuario normal y pasarlo a administrador del sistema, tambien se modifica usuario.js agregando detalles en cuanto a las buenas practicas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 
 import { HomeModule } from './home/home.module';
 import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { LoginComponent } from './login/login.component';
     ProfileComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
