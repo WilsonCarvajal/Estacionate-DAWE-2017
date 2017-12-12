a2ef6e3c8ac2c3cf7937b88e59ac1769622410a4
blacksinx <tas001@alumnos.ucn.cl>
2017-12-12 09:58:05 -0300

    Added: se agrega midleware

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -5,10 +5,11 @@
 
 var express = require('express');
 var UsuarioController = require('../controllers/usuario');
+const mdAuth = require('../../../../DAWE/Estacionate-DAWE-2017/server/midleware/autenticator');
 
 var api = express.Router();
 
-api.get('/prueba', UsuarioController.prueba_usuario);
+api.get('/prueba', mdAuth.ensureAuth, UsuarioController.prueba_usuario);
 api.post('/registro', UsuarioController.guardar_usuario);
 api.get('/buscar', UsuarioController.buscar_usuario);
 api.post('/iniciarSesion', UsuarioController.inicio_sesion);
