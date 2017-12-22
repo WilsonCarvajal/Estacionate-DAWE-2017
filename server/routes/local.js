3f5725e847179894f41cfc9e4925da28a198b9e8
blacksinx <tas001@alumnos.ucn.cl>
2017-12-22 14:42:19 -0300

    Added: se agrega midleware a las rutas de locales

--- server/routes/local.js
+++ server/routes/local.js
@@ -3,12 +3,13 @@
 let express = require('express');
 
 let LocalController = require('../controllers/local');
+const mdAuth = require('../midleware/autenticator');
 let api = express.Router();
 
 
-api.post('/registro_local', LocalController.guardar_local);
+api.post('/registro_local', mdAuth.ensureAuth,LocalController.guardar_local);
 api.post('/buscar_local', LocalController.buscar_locales_por_coordenadas);
-api.get('/prueba_local', LocalController.prueba_local);
-api.post('/agregar_estacionamiento', LocalController.agregarEstacionamientos);
+api.get('/prueba_local', mdAuth.ensureAuth,LocalController.prueba_local);
+api.post('/agregar_estacionamiento', mdAuth.ensureAuth,LocalController.agregarEstacionamientos);
 
 module.exports = api;
